b0cc28e393117b93fb64b83706b0c43063b4711b
Maxur <Marcelo.soto01@alumnos.ucn.cl>
2020-11-12 18:47:57 -0300

    ahhhhhhhhhhh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    probando otra vez
+                    AHHHHHHHHHHHHHHHHHHHHHHH
                 </div>
 
                 <div class="links">
