631751321bf6fe68dea417aab188c0a31ac9f7f0
Maxur <Marcelo.soto01@alumnos.ucn.cl>
2020-11-12 18:20:31 -0300

    probando

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Probando
+                    probando otra vez
                 </div>
 
                 <div class="links">
