0aa3cbda1c6d420b27976ff743d7c2b860cb9d8c
Sebastian Murquio <sebastian.murquio@alumnos.ucn.cl>
2020-11-12 17:46:48 -0300

    Prueba version NekoDarkZ
    se realizo un cambio en el welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    NekoDarkZ
                 </div>
 
                 <div class="links">
